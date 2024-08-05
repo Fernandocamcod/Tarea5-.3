6f8d2763189521e72b5563a92976c123df6aa818
Fernandocamcod <williamfernando1981@gmail.com>
2024-08-05 17:18:34 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero = 10;
+
+        // Bucle do-while para imprimir la secuencia de números de 10 a 0
+        do
+        {
+            Console.WriteLine(numero);
+            numero--;
+        } while (numero >= 0);
+
+        // Mantiene la consola abierta hasta que el usuario presione una tecla
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
